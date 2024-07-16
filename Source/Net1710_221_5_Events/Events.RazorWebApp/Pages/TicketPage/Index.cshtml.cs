9ffe56a7f4b42960f1ca284755bc4f182a0f1b84
totrolf <minhtoan1696@gmail.com>
2024-07-16 23:14:25 +0700

    fix search

--- Source/Net1710_221_5_Events/Events.RazorWebApp/Pages/TicketPage/Index.cshtml.cs
+++ Source/Net1710_221_5_Events/Events.RazorWebApp/Pages/TicketPage/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Events.Data.Models;
 using Events.Business.Business;
 using Events.Common;
+using Microsoft.CodeAnalysis.Options;
 
 namespace Events.RazorWebApp.Pages.TicketPage
 {
@@ -27,13 +28,21 @@ namespace Events.RazorWebApp.Pages.TicketPage
         public int TotalPages { get; set; }
         // Take 5 records per page
         public const int PageSize = 5;
-
         // Searching component
         [BindProperty(SupportsGet = true)]
-        public string SearchField { get; set; }
+        public string SearchCode { get; set; }
 
         [BindProperty(SupportsGet = true)]
-        public string SearchTerm { get; set; }
+        public string SearchPartName { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchPartMail { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchPartPhone { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchEventName { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public DateTime? StartDate { get; set; }
@@ -48,17 +57,25 @@ namespace Events.RazorWebApp.Pages.TicketPage
             if (data.Status == Const.SUCCESS_READ_CODE && data.Data != null)
             {
                 var tickets = (List<Ticket>)data.Data;
-                if (!string.IsNullOrEmpty(SearchTerm))
+                if (!string.IsNullOrEmpty(SearchCode))
                 {
-                    tickets = SearchField switch
-                    {
-                        "Code" => tickets.Where(o => o.Code.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)).ToList(),
-                        "PartName" => tickets.Where(o => o.ParticipantName.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)).ToList(),
-                        "PartMail" => tickets.Where(o => o.ParticipantMail.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)).ToList(),
-                        "PartPhone" => tickets.Where(o => o.ParticipantPhone.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)).ToList(),
-                        "EventName" => tickets.Where(o => o.OrderDetail.Event.Name.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)).ToList(),
-                        _ => tickets
-                    };
+                    tickets = tickets.Where(o => o.Code.Contains(SearchCode, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+                if (!string.IsNullOrEmpty(SearchPartName))
+                {
+                    tickets = tickets.Where(o => o.ParticipantName.Contains(SearchPartName, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+                if (!string.IsNullOrEmpty(SearchPartMail))
+                {
+                    tickets = tickets.Where(o => o.ParticipantMail.Contains(SearchPartMail, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+                if (!string.IsNullOrEmpty(SearchPartPhone))
+                {
+                    tickets = tickets.Where(o => o.ParticipantPhone.Contains(SearchPartPhone, StringComparison.OrdinalIgnoreCase)).ToList();
+                }
+                if (!string.IsNullOrEmpty(SearchEventName))
+                {
+                    tickets = tickets.Where(o => o.OrderDetail.Event.Name.Contains(SearchEventName, StringComparison.OrdinalIgnoreCase)).ToList();
                 }
                 // Apply search from date to date
                 if (StartDate.HasValue && EndDate.HasValue)
